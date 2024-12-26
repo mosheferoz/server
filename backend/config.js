d8904a38bf11e8b113492616c1a879ebe4632695
mosheferoz <mosheferoz@gmail.com>
2024-12-26 18:58:02 +0200

    הודעה שמסבירה את השינויים שביצעת

--- backend/config.js
+++ backend/config.js
@@ -8,8 +8,7 @@ const firebaseConfig = process.env.FIREBASE_PROJECT_ID ? {
 
 module.exports = {
   server: {
-    port: process.env.PORT || 3000,
-    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
+    port: parseInt(process.env.PORT) || 3000,
     env: process.env.NODE_ENV || 'development'
   },
   firebase: firebaseConfig,
